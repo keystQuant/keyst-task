151b807e8fd8e5bdf50ecceb124d42de6c3f12eb
1 <ppark9553@gmail.com>
2018-10-05 20:03:20 +0900

    태스크 분배 시스템 코드 업데이트 완료

---
+++ keystone/settings.py
@@ -0,0 +1,98 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'k8k2-sr=hk^x5@@b%n!8)kf)9!t(0e-t$vj8dq9gz52isr4a6n'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # celery + celerybeat
+    'django_celery_beat',
+    'django_celery_results',
+
+    # apps
+    'task',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'keystone.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'keystone.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'ko-kr'
+TIME_ZONE = 'Asia/Seoul'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = False
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+amqp_user = 'admin'
+amqp_pass = 'admin123'
+amqp_location = 'rabbit'
+amqp_url = 'amqp://{}:{}@{}:5672//'.format(amqp_user, amqp_pass, amqp_location)
+
+CELERY_BROKER_URL = amqp_url
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
---
+++ task/tasks.py
@@ -0,0 +1,66 @@
+from celery import shared_task
+
+import time
+import requests
+from .cache import RedisClient
+
+GOBBLE_URL = 'http://45.77.31.8:3000/task/'
+API_URL = 'http://45.76.202.71:3000/api/v1/stocks/task/?type='
+
+def update_tasks(redis_client):
+    task_ran = redis_client.key_exists('TASK_IN_PROGRESS')
+    if not task_ran:
+        return 1
+
+    task_in_progress = redis_client.get_key('TASK_IN_PROGRESS')
+    task_in_progress = 1 if task_in_progress == 'True' else 0
+    if not task_ran or not task_in_progress:
+        # task가 실행중이 아니면, 실행
+        return 1
+    else:
+        return 0
+
+def starting_tasks(redis_client):
+    redis_client.set_key('TASK_IN_PROGRESS', 'True')
+
+def ending_tasks(redis_client):
+    redis_client.set_key('TASK_IN_PROGRESS', 'False')
+
+@shared_task
+def data_update_task():
+    print('데이터 수집 작업 시작')
+
+    r = RedisClient()
+    update = update_tasks(r)
+
+    if update == 1:
+        print('같은 프로세스가 현재 실행중이지 않습니다. 태스크 시작')
+        starting_tasks(r)
+        ### 날짜부터 업데이트한다
+        ### 하지만 날짜가 업데이트되기 전에 레디스에서 그 상태를 알 수 있도록 업데이트 안 되었다고 한다
+        print('날짜 정보를 수집합니다')
+        r.set_key('DATE_JUST_UPDATED_TO_DB', 'False')
+        task = requests.get(GOBBLE_URL + 'DATE')
+
+        set_other_tasks_ready = False
+        while not set_other_tasks_ready:
+            date_just_updated_in_db = r.get_key('DATE_JUST_UPDATED_TO_DB')
+            if date_just_updated_in_db == 'True':
+                print('날짜가 업데이트되었습니다. 다음 태스크로 넘어갑니다.')
+                set_other_tasks_ready = True
+            else:
+                print('dates still not updated, waiting...')
+                continue
+            time.sleep(4) # 4초씩 쉬고 레디스 서버에 요청을 보낸다
+
+        print('업데이트가 필요한 정보를 DB에서 확인합니다.')
+        task = requests.get(API_URL + 'SET_UPDATE_TASKS')
+
+        if task.json()['status'] == 'DONE':
+            # 날짜가 업데이트되었다면, 업데이트할 모든 데이터를 FnGuide로부터 수집해온다
+            task = requests.get(GOBBLE_URL + 'FNGUIDE')
+
+        ending_tasks(r)
+    else:
+        print('TASK ALREADY IN PROGRESS: 같은 태스크가 이미 실행중입니다. 조금 있다 다시 시도합니다.')
+    return True
