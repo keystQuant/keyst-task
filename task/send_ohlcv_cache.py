850481dd70d72d47661cb637ba6385b6ebe95cc2
MH-Lee <lmhoon012@gmail.com>
2019-01-06 15:48:44 +0900

    send-cache problem solve

--- task/send_ohlcv_cache.py
+++ task/send_ohlcv_cache.py
@@ -16,21 +16,23 @@ class KeystTask(object):
 
     def __init__(self):
         self.mktcap_url = 'http://45.76.202.71:3000/api/v1/stocks/mktcap/?date={}&page={}'
-        self.today_date = time.strftime('%Y%m%d')
         self.cache_ip = '198.13.60.19'
         self.cache_pw = 'da56038fa453c22d2c46e83179126e97d4d272d02ece83eb83a97357e842d065'
         self.r = redis.StrictRedis(host=self.cache_ip, port=6379, password=self.cache_pw)
         self.kp_tickers = [ticker.decode() for ticker in self.r.lrange(KOSPI_TICKERS, 0 ,-1)]
         self.kd_tickers = [ticker.decode() for ticker in self.r.lrange(KOSDAQ_TICKERS, 0 ,-1)]
-        print("Task is ready")
+        print("Task is ready", len(self.kp_tickers), len(self.kd_tickers))
+
 
     def make_refined_data(self):
+        samsung_ohlcv = pd.read_msgpack(self.r.get('005930_OHLCV'))
+        recent_date = int(samsung_ohlcv.tail(1)['date'])
         refined_ticker = []
         status_code = 200
         i = 0
         while True:
             i += 1
-            req = requests.get(self.mktcap_url.format(self.today_date, i))
+            req = requests.get(self.mktcap_url.format(recent_date, i))
             status_code = req.status_code
             if status_code == 404:
                 break
@@ -58,9 +60,12 @@ class KeystTask(object):
             tickers_list =  kd_tickers_list
         else:
             print('choose kp or kd')
+        i = 0
         for ticker in tickers_list:
             # OHLCV 데이터 불러오기
-            print(ticker)
+            i += 1
+            if i % 100 == 0:
+                print(ticker)
             key = ticker + '_OHLCV'
             ohlcv = pd.read_msgpack(self.r.get(key))
             ohlcv.set_index('date', inplace=True)
@@ -77,6 +82,7 @@ class KeystTask(object):
             else:
                 total_ohlcv = pd.concat([total_ohlcv, ohlcv_df], axis=1)
                 total_vol = pd.concat([total_vol, vol_df], axis=1)
+            print(len(total_ohlcv, total_vol))
         return total_ohlcv, total_vol
 
     def send_ohlcv_data(self):
